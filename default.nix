667ec7106fddf24affc94184c44a0b3a8e23a267
Oleg Pykhalov <go.wigust@gmail.com>
2019-08-08 14:27:06 +0300

    Use php55Packages from overlay

--- default.nix
+++ default.nix
@@ -109,38 +109,6 @@ sh = dash.overrideAttrs (_: rec {
   postInstall = ''(cd "$out/include" && ln -s ImageMagick* ImageMagick)'';
  };
 
-buildPhp55Package = args: buildPhpPackage ({ php = php55; } // args);
-
-
-php55Packages = {
-  timezonedb = buildPhp55Package {
-    name = "timezonedb";
-    version = "2019.1";
-    sha256 = "0rrxfs5izdmimww1w9khzs9vcmgi1l90wni9ypqdyk773cxsn725";
-  };
-
-  dbase = buildPhp55Package {
-      name = "dbase";
-      version = "5.1.0";
-      sha256 = "15vs527kkdfp119gbhgahzdcww9ds093bi9ya1ps1r7gn87s9mi0";
-  };
-
-  intl = buildPhp55Package {
-      name = "intl";
-      version = "3.0.0";
-      sha256 = "11sz4mx56pc1k7llgbbpz2i6ls73zcxxdwa1d0jl20ybixqxmgc8";
-      inputs = [ icu58 ];
-  };
-
-  imagick = buildPhp55Package {
-      name = "imagick";
-      version = "3.1.2";
-      sha256 = "528769ac304a0bbe9a248811325042188c9d16e06de16f111fee317c85a36c93";
-      inputs = [ pkgconfig imagemagick68 pcre ];
-      configureFlags = [ "--with-imagick=${imagemagick68}" ];
-  };
-};
-
   rootfs = mkRootfs {
       name = "apache2-php55-rootfs";
       src = ./rootfs;
