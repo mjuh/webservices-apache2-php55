ee3f42394ece35425e11577a08471a899cba3d37
Андрей Здетовецкий <zdetovetskiy@majordomo.ru>
2019-08-09 19:15:13 +0300

    perl!!!

--- default.nix
+++ default.nix
@@ -27,7 +27,8 @@ sh = dash.overrideAttrs (_: rec {
       name = "apache2-php55-rootfs";
       src = ./rootfs;
       inherit curl coreutils findutils apacheHttpdmpmITK apacheHttpd
-        mjHttpErrorPages php55 postfix s6 execline mjperl5Packages ;
+        mjHttpErrorPages php55 postfix s6 execline ;
+      mjperl5Packages = mjperl5lib;
       ioncube = ioncube.v55;
       zendguard = zendguard55;
       s6PortableUtils = s6-portable-utils;
@@ -78,33 +79,9 @@ pkgs.dockerTools.buildLayeredImage rec {
     postfix
     sh
     coreutils
-    perl
-         perlPackages.TextTruncate
-         perlPackages.TimeLocal
-         perlPackages.PerlMagick
-         perlPackages.commonsense
-         perlPackages.Mojolicious
-         perlPackages.base
-         perlPackages.libxml_perl
-         perlPackages.libnet
-         perlPackages.libintl_perl
-         perlPackages.LWP
-         perlPackages.ListMoreUtilsXS
-         perlPackages.LWPProtocolHttps
-         perlPackages.DBI
-         perlPackages.DBDmysql
-         perlPackages.CGI
-         perlPackages.FilePath
-         perlPackages.DigestPerlMD5
-         perlPackages.DigestSHA1
-         perlPackages.FileBOM
-         perlPackages.GD
-         perlPackages.LocaleGettext
-         perlPackages.HashDiff
-         perlPackages.JSONXS
-         perlPackages.POSIXstrftimeCompiler
-         perlPackages.perl
-  ] ++ collect isDerivation php55Packages;
+  ]
+  ++ collect isDerivation mjperl5Packages
+  ++ collect isDerivation php55Packages;
   config = {
     Entrypoint = [ "${rootfs}/init" ];
     Env = [
