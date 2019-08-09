ebc65d67adaff2ca3ca30f38a995c15dcb09f87f
Oleg Pykhalov <go.wigust@gmail.com>
2019-08-09 11:39:42 +0300

    Move most of packages to overlay

--- default.nix
+++ default.nix
@@ -8,7 +8,7 @@ let
 
 inherit (builtins) concatMap getEnv toJSON;
 inherit (dockerTools) buildLayeredImage;
-inherit (lib) concatMapStringsSep firstNChars flattenSet dockerRunCmd buildPhpPackage mkRootfs;
+inherit (lib) concatMapStringsSep firstNChars flattenSet dockerRunCmd mkRootfs;
 inherit (lib.attrsets) collect isDerivation;
 inherit (stdenv) mkDerivation;
 
@@ -23,96 +23,11 @@ sh = dash.overrideAttrs (_: rec {
   '';
 });
 
-  pcre831 = stdenv.mkDerivation rec {
-      name = "pcre-8.31";
-      src = fetchurl {
-          url = "https://ftp.pcre.org/pub/pcre/${name}.tar.bz2";
-          sha256 = "0g4c0z4h30v8g8qg02zcbv7n67j5kz0ri9cfhgkpwg276ljs0y2p";
-      };
-      outputs = [ "out" ];
-      configureFlags = ''
-          --enable-jit
-      '';
-  };
-
-  libjpeg130 = stdenv.mkDerivation rec {
-     name = "libjpeg-turbo-1.3.0";
-     src = fetchurl {
-         url = "mirror://sourceforge/libjpeg-turbo/${name}.tar.gz";
-         sha256 = "0d0jwdmj3h89bxdxlwrys2mw18mqcj4rzgb5l2ndpah8zj600mr6";
-     };
-     buildInputs = [ nasm ];
-     doCheck = true;
-     checkTarget = "test";
- };
-
-  libpng12 = stdenv.mkDerivation rec {
-     name = "libpng-1.2.59";
-     src = fetchurl {
-        url = "mirror://sourceforge/libpng/${name}.tar.xz";
-        sha256 = "b4635f15b8adccc8ad0934eea485ef59cc4cae24d0f0300a9a941e51974ffcc7";
-     };
-     buildInputs = [ zlib ];
-     doCheck = true;
-     checkTarget = "test";
-  };
-
-  connectorc = stdenv.mkDerivation rec {
-     name = "mariadb-connector-c-${version}";
-     version = "6.1.0";
-
-     src = fetchurl {
-         url = "https://downloads.mysql.com/archives/get/file/mysql-connector-c-6.1.0-src.tar.gz";
-         sha256 = "0cifddg0i8zm8p7cp13vsydlpcyv37mz070v6l2mnvy0k8cng2na";
-         name   = "mariadb-connector-c-${version}-src.tar.gz";
-     };
-
-  # outputs = [ "dev" "out" ]; FIXME: cmake variables don't allow that < 3.0
-     cmakeFlags = [
-            "-DWITH_EXTERNAL_ZLIB=ON"
-            "-DMYSQL_UNIX_ADDR=/run/mysqld/mysqld.sock"
-     ];
-
-  # The cmake setup-hook uses $out/lib by default, this is not the case here.
-     preConfigure = stdenv.lib.optionalString stdenv.isDarwin ''
-             cmakeFlagsArray+=("-DCMAKE_INSTALL_NAME_DIR=$out/lib/mariadb")
-     '';
-
-     nativeBuildInputs = [ cmake ];
-     propagatedBuildInputs = [ openssl zlib ];
-     buildInputs = [ libiconv ];
-     enableParallelBuilding = true;
-  };
-
- imagemagick68 = stdenv.mkDerivation rec {
-  version = "6.8.8-7";
-    name = "ImageMagick-${version}";
-
-  src = fetchurl {
-    url = "https://mirror.sobukus.de/files/src/imagemagick/${name}.tar.xz";
-    sha256 = "1x5jkbrlc10rx7vm344j7xrs74c80xk3n1akqx8w5c194fj56mza";
-  };
-
-  enableParallelBuilding = true;
-
-  configureFlags = ''
-    --with-gslib
-    --with-frozenpaths
-    ${if librsvg != null then "--with-rsvg" else ""}
-  '';
-
-  buildInputs =
-    [ pkgconfig bzip2 fontconfig freetype libjpeg libpng libtiff libxml2 zlib librsvg
-      libtool jasper
-    ];
-
-  postInstall = ''(cd "$out/include" && ln -s ImageMagick* ImageMagick)'';
- };
-
   rootfs = mkRootfs {
       name = "apache2-php55-rootfs";
       src = ./rootfs;
-      inherit curl coreutils findutils apacheHttpdmpmITK apacheHttpd mjHttpErrorPages php55 postfix s6 execline connectorc mjperl5Packages ;
+      inherit curl coreutils findutils apacheHttpdmpmITK apacheHttpd
+        mjHttpErrorPages php55 postfix s6 execline mjperl5Packages ;
       ioncube = ioncube.v55;
       s6PortableUtils = s6-portable-utils;
       s6LinuxUtils = s6-linux-utils;
