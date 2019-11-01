141bb83bed4c424d3cbc84e8bb5fbebcaa803939
Oleg Pykhalov <go.wigust@gmail.com>
2019-11-01 18:29:08 +0300

    default.nix: Collect isDerivation mjperl5Packages.

--- default.nix
+++ default.nix
@@ -47,7 +47,9 @@ pkgs.dockerTools.buildLayeredImage rec {
     (optipng.override{ inherit libpng ;})
     gifsicle cacert zip
     perl
-  ] ++ collect isDerivation phpDeprecatedPackages.php55Packages;
+  ]
+  ++ collect isDerivation phpDeprecatedPackages.php55Packages
+  ++ collect isDerivation mjperl5Packages;
   config = {
     Entrypoint = [ "${rootfs}/init" ];
     Env = [
